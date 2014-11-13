14dc6544e9af375362472e3eeb612af86e31bd4b
Lara Finnegan <lcfinnegan@gmail.com>
2014-11-12 21:28:25 -0600

    can now delete tags

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -2,4 +2,15 @@ class TagsController < ApplicationController
 	def show
 		@tag = Tag.find(params[:id])
 	end
+
+	def index
+		@tags = Tag.all
+	end
+
+	def destroy
+		@tag = Tag.find(params[:id])
+		@tag.delete
+		flash.notice = "Tag '#{@tag.name}' has been deleted!"
+		redirect_to tags_path
+	end
 end
